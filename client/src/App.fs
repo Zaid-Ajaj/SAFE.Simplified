1efcb4bdae7ad245afc9f86cdf194e11a90ae375
Zaid <zaid.naom@gmail.com>
2020-03-07 03:51:20 +0100

    Use static welcome message in home page

--- client/src/App.fs
+++ client/src/App.fs
@@ -73,7 +73,7 @@ let render (state: State) (dispatch: Msg -> unit) =
                 prop.width 250
             ]
 
-            Html.h1 (Config.variable "WELCOME_MESSAGE")
+            Html.h1 "Full-Stack Counter"
             Html.button [
                 prop.onClick (fun _ -> dispatch Increment)
                 prop.text "Increment"
